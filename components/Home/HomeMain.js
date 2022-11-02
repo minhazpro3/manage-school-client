d3f97b9f76732f3bf9b587d32cef9492de058299
minhazpro3 <minhazpro1@gmail.com>
2022-11-02 23:47:08 +0600

    first commit

---
+++ components/Home/HomeMain.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Banner from '../HomeComponents/Banner'
+
+const HomeMain = () => {
+  return (
+    <div><Banner/></div>
+  )
+}
+
+export default HomeMain
---
+++ components/HomeComponents/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+    {/* water waves */}
+    <div >
+    <div className="Banner_body ">
+       <div className="Banner_waves  ">
+        <div className="waves">
+        <div className="wave" id="wave_1"></div>
+        <div className="wave" id="wave_2"></div>
+        <div className="wave" id="wave_3"></div>
+        <div className="wave" id="wave_4"></div>
+        </div>
+       </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Banner
